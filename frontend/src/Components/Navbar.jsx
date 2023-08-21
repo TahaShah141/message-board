51ed8c9d41944df7d5ae38f54a8771ec6f3c971f
Taha Shah <tahashah411@gmail.com>
2023-08-22 04:51:57 +0500

    make responsive navbar animate

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -30,14 +30,14 @@ export default function Navbar() {
         <button onClick={() => setOpen(!isOpen)}className="text-white w-10 h-10"><svg className=" w-full h-full" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" fill="currentColor"/></svg></button>
         {isOpen && (
         <>
-          {user && (<div className="absolute bg-primary top-full left-[5%] w-[90%] my-4 rounded-3xl border-4 border-neutral-900 p-3 text-lg flex flex-col items-center gap-4">
+          {user && (<div className="absolute bg-primary top-full left-[5%] w-[90%] my-4 rounded-3xl border-4 border-neutral-900 p-3 text-lg flex flex-col items-center gap-4 animate-popup">
           <NavLink className="nav-link" onClick={() => setOpen(false)} to="/">Home</NavLink>
           <NavLink className="nav-link" onClick={() => setOpen(false)} to="/messages">Messages</NavLink>
           <NavLink className="nav-link" onClick={() => setOpen(false)} to="/profile">Profile</NavLink>
           <button className="font-mono border-2 border-white p-2 text-sm rounded-md bg-neutral-700 hover:scale-110 hover:font-bold hover:border-black hover:bg-red-500" onClick={() => dispatch({type: 'LOGOUT'})}>Log Out</button>
           </div>)}
 
-          {!user && (<div className="absolute bg-primary top-full left-[5%] w-[90%] my-4 rounded-3xl border-4 border-neutral-900 p-3 text-lg flex flex-col items-center gap-4">
+          {!user && (<div className="absolute bg-primary top-full left-[5%] w-[90%] my-4 rounded-3xl border-4 border-neutral-900 p-3 text-lg flex flex-col items-center gap-4 animate-popup">
           <NavLink className="nav-link" onClick={() => setOpen(false)} to="/signup">Sign Up</NavLink>
           <NavLink className="nav-link" onClick={() => setOpen(false)} to="/login">Login</NavLink>
           </div>)}
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,6 +8,32 @@ export default {
     extend: {
       backgroundColor: {
         primary: "#333333"
+      },
+
+      animation: {
+        'popup': 'fade-in 1s linear forwards'
+      },
+
+      keyframes: {
+        'fade-in': {
+          'from': {
+            transform: 'translateY(-10px)',
+            opacity: '0'
+          },
+          
+          '25%': {
+            transform: 'translateY(5px)',
+          },
+          
+          '50%': {
+            opacity: '1',
+            transform: 'translateY(-2px)',
+          },
+
+          'to': {
+            transform: 'translateY(0)',
+          }
+        }
       }
     },
   },
