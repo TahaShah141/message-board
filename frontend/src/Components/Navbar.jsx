8d5171eed14b6595beeefe0715f92daa14860a1d
Taha Shah <tahashah411@gmail.com>
2023-08-22 03:55:42 +0500

    make navbar responsive

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,12 +1,16 @@
 import { NavLink } from "react-router-dom";
 import { useAuthContext } from "../Hooks/useAuthContext";
+import { useState } from "react";
 
 export default function Navbar() {
 
   const { user, dispatch } = useAuthContext()
 
+  const [isOpen, setOpen] = useState(false)
+
   return (
-    <div className="flex justify-between items-center p-5 bg-primary text-white md:px-10">
+    <>
+    <div className="hidden justify-between items-center p-7 bg-primary text-white md:px-10 sm:flex">
         <h1 className="text-2xl font-bold md:text-5xl">Board<span className="text-red-500 inline-block -rotate-12 translate-x-1 -translate-y-1">em</span></h1>
         {user && (<div className="flex gap-4 text-lg md:gap-9 md:text-xl">
         <NavLink className="nav-link" to="/">Home</NavLink>
@@ -20,5 +24,26 @@ export default function Navbar() {
         <NavLink className="nav-link" to="/login">Login</NavLink>
         </div>)}
     </div>
+
+    <div className="relative flex justify-between items-center p-7 bg-primary text-white md:px-10 sm:hidden">
+        <h1 className="text-2xl font-bold md:text-5xl">Board<span className="text-red-500 inline-block -rotate-12 translate-x-1 -translate-y-1">em</span></h1>
+        <button onClick={() => setOpen(!isOpen)}className="text-white w-10 h-10"><svg className=" w-full h-full" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" fill="currentColor"/></svg></button>
+        {isOpen && (
+        <>
+          {user && (<div className="absolute bg-primary top-full left-[5%] w-[90%] my-4 rounded-3xl border-4 border-neutral-900 p-3 text-lg flex flex-col items-center gap-4">
+          <NavLink className="nav-link" onClick={() => setOpen(false)} to="/">Home</NavLink>
+          <NavLink className="nav-link" onClick={() => setOpen(false)} to="/messages">Messages</NavLink>
+          <NavLink className="nav-link" onClick={() => setOpen(false)} to="/profile">Profile</NavLink>
+          <button className="font-mono border-2 border-white p-2 text-sm rounded-md bg-neutral-700 hover:scale-110 hover:font-bold hover:border-black hover:bg-red-500" onClick={() => dispatch({type: 'LOGOUT'})}>Log Out</button>
+          </div>)}
+
+          {!user && (<div className="absolute bg-primary top-full left-[5%] w-[90%] my-4 rounded-3xl border-4 border-neutral-900 p-3 text-lg flex flex-col items-center gap-4">
+          <NavLink className="nav-link" onClick={() => setOpen(false)} to="/signup">Sign Up</NavLink>
+          <NavLink className="nav-link" onClick={() => setOpen(false)} to="/login">Login</NavLink>
+          </div>)}
+        </>
+        )}
+    </div>
+    </>
   )
 }
